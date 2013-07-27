1f47111f00cb9e491560aba357a93750e642e58f
Thomas Rampelberg <thomas@saunter.org>
2013-07-27 12:52:00 -0700

    Making the list filter instead of showing suggestions

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,35 @@
 
+// An implementation of a case-insensitive contains pseudo
+// made for all versions of jQuery
+(function( $ ) {
+
+function icontains( elem, text ) {
+  return (
+    elem.textContent ||
+    elem.innerText ||
+    $( elem ).text() ||
+    ""
+  ).toLowerCase().indexOf( (text || "").toLowerCase() ) > -1;
+}
+
+$.expr[':'].icontains = $.expr.createPseudo ?
+  $.expr.createPseudo(function( text ) {
+    return function( elem ) {
+        return icontains( elem, text );
+    };
+}) :
+function( elem, i, match ) {
+  return icontains( elem, match[3] );
+};
+
+})( jQuery );
+
 $(document).ready(function() {
 	var elem = $("input[name=name]");
-	elem.typeahead({ source: _.keys(attendees) });
-	elem.change(function() {
-		$("input[name=member_id]").val(attendees[elem.val()]);
+	elem.keyup(function() {
+    $(".member").hide();
+
+    var name = $(this).val();
+    $(".name:icontains(" + name + ")").parents(".member").show();
 	});
 });
